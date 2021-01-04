b498b02b3be13ebdb0205da50fa656dac4d2cd6b
losos0 <losos0@users.noreply.github.com>
2021-01-04 05:52:17 -0600

    Support zsh

--- bashrc.sh
+++ bashrc.sh
@@ -1,3 +1,6 @@
 alias ls='ls -A'
+# bash
 PROMPT_COMMAND="echo -n \$(date '+%Y-%m-%d %H:%M:%S %Z') $$ >> ~/hist.txt; fc -ln -1 >> ~/hist.txt"
+# zsh
+precmd() { eval "$PROMPT_COMMAND"; }
 gh() { grep $* ~/hist.txt; }
--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
-touch ~/.bashrc
-grep -q '^source .*bashrc\.sh$' ~/.bashrc && echo 'already installed?' && exit
-printf 'source %q\n' "$(dirname "$(realpath "$0")")/bashrc.sh" >> ~/.bashrc
+if [ -n "$ZSH_NAME" ]; then
+    rc="$HOME/.zshrc"
+elif [ -n "$BASH" ]; then
+    rc="$HOME/.bashrc"
+else
+    echo 'Unknown shell' && exit 1
+fi
+
+touch "$rc"
+grep -q '^source .*bashrc\.sh$' "$rc" && echo 'already installed?' && exit
+printf 'source %q\n' "$(cd "$(dirname "$0")"; pwd)/bashrc.sh" >> "$rc"
