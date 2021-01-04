ba214acd725f76584005899db996c789b567024b
losos0 <losos0@users.noreply.github.com>
2021-01-04 06:35:54 -0600

    zsh - use preexec
    Trying to reuse the bash one sucked because fc didn't print any leading
    spaces

--- bashrc.sh
+++ bashrc.sh
@@ -2,5 +2,7 @@ alias ls='ls -A'
 # bash
 PROMPT_COMMAND="echo -n \$(date '+%Y-%m-%d %H:%M:%S %Z') $$ >> ~/hist.txt; fc -ln -1 >> ~/hist.txt"
 # zsh
-precmd() { eval "$PROMPT_COMMAND"; }
+preexec() {
+    echo $(date '+%Y-%m-%d %H:%M:%S %Z') $$ " $1" >> ~/hist.txt
+}
 gh() { grep $* ~/hist.txt; }
