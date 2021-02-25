a37e0be3f404adffe6258f657b1b755caf2b9bb3
losos0 <losos0@users.noreply.github.com>
2021-02-24 22:25:37 -0600

    Fix bash installation

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
+# Install with <shell> install.sh
 if [ -n "$ZSH_NAME" ]; then
     rc="$HOME/.zshrc"
+    script="$1"
 elif [ -n "$BASH" ]; then
     rc="$HOME/.bashrc"
+    script="$0"
 else
     echo 'Unknown shell' && exit 1
 fi
 
 touch "$rc"
 grep -q '^source .*bashrc\.sh$' "$rc" && echo 'already installed?' && exit
-printf 'source %q\n' "$(cd "$(dirname "$1")"; pwd)/bashrc.sh" >> "$rc"
+printf 'source %q\n' "$(cd "$(dirname "$script")"; pwd)/bashrc.sh" >> "$rc"
